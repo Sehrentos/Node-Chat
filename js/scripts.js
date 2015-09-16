a35fffa17a30c426e90bbb814b0812827f93acb1
Sehrentos <sehrentos@gmail.com>
2015-09-16 15:53:38 +0300

    Edit & clean
    Edited draw menu function and how user will be asked about starting own
    server on start.

---
+++ js/scripts.js
@@ -0,0 +1,849 @@
+/*
+* scripts.js - default javascripts
+*
+* Define chat object:
+*/
+var chat = {
+	user: {},
+	method: 'http', // https, http, ws
+	address: 'localhost',
+	port: 9000,
+	channel: '',
+	debug_mode: false
+};
+
+/*
+* Debug function: chat.debug()
+* Enable / disable by changing chat.debug = true/false
+* @require String
+*/
+chat.debug = function(str) {
+	return !chat.debug_mode || console.log(str);
+};
+
+/*
+* WebSocket init
+* @require: url - optional
+*
+* @urlSource: @http://socket.io/docs/client-api/
+*/
+chat.init = function(url) {
+	var self = this;
+	var link = url || chat.method +'://'+ chat.address +':'+ chat.port + chat.channel;
+
+	// Open socket
+	self.socket = io.connect(link);
+
+	// Successful connection
+	self.socket.on('connect', function (data) {
+		self.setName();
+	});
+
+	// Error
+	self.socket.on('error', function (data) {
+		chat.debug(data);
+	});
+
+	// Connect error
+	self.socket.on('connect_error', function (data) {
+		chat.debug(data);
+		if (data.type === "TransportError") {
+			self.mes("Unable to connect server.");
+			// Clear user menu
+			self.menuClear();
+			self.socket.close();
+		}
+	});
+
+	// Time out
+	self.socket.on('connect_timeout', function (data) {
+		chat.debug(data);
+		self.mes("Connection timeout.");
+		// Clear user menu
+		self.menuClear();
+	});
+
+	// Disconnect
+	self.socket.on('disconnect', function (data) {
+		chat.debug(data);
+		self.mes("Disconnected from server.");
+		// Clear user menu
+		self.menuClear();
+		// Ask to reconnect
+		$.fn.nConfirm({
+			title: "Disconnected!",
+			message: "Do you wan't to <b>reconnect</b> to the server?",
+			enableBackground: false,
+			onSubmit: function(str) {
+				self.socket.connect();
+			}
+		});
+	});
+
+	// Welcome
+	self.socket.on('header-topic', function (data) {
+		chat.debug('header-topic:');
+		chat.debug(data);
+		//self.mes(data.message);
+		$("#header").find("#topic").html(data.message);
+	});
+
+	// Notice
+	self.socket.on('notice', function (data) {
+		chat.debug('Notice:');
+		chat.debug(data);
+		self.mes(data.message);
+	});
+
+	// Update user data
+	self.socket.on('update-user', function (data) {
+		chat.debug('Update-user:');
+		chat.debug(data);
+		self.user = data;
+	});
+
+
+	// New updater: add user
+	self.socket.on('channel-user-add', function (data) {
+		chat.debug('Channel-user-add:');
+		chat.debug(data);
+		self.menuAddUser(data.id, data.name, data.channel);
+	});
+
+	// New updater: add user
+	self.socket.on('channel-user-list', function (data) { //FIXME
+		chat.debug('Channel-user-list:');
+		chat.debug(data);
+		//self.menuAddUser(data.id, data.name, data.channel);
+		self.updateUsers(data);
+	});
+
+	//New updater: remove user
+	self.socket.on('channel-user-remove', function (data) {
+		chat.debug('Channel-user-remove:');
+		chat.debug(data);
+		self.menuRemoveUser(data.id); //userid
+	});
+
+	// New updater: user update
+	self.socket.on('channel-user-update', function (data) {
+		chat.debug('Channel-user-update:');
+		chat.debug(data);
+		self.menuUpdateUser(data.id, data.name, data.channel);
+	});
+
+	// Whisper
+	self.socket.on('whisper', function (data) {
+		chat.debug('Whisper:');
+		chat.debug(data);
+		var d = new Date(data.date),
+			hours = d.getHours(),
+			minutes = d.getMinutes(),
+			seconds = d.getSeconds();
+
+		var msg = "["+ self.twoDigits(hours) + ":" + self.twoDigits(minutes) + ":" + self.twoDigits(seconds) +"] ";
+		if (data.from === data.to || data.from === self.user.name) {
+			msg += "Whisper to <a href=\"javascript:void(0)\" class=\"nickname\" onclick=\"chat.setWhisper('"+ data.to +"')\">&lt;"+ data.to +"&gt;</a> ";
+		} else {
+			msg += "<a href=\"javascript:void(0)\" class=\"nickname\" onclick=\"chat.setWhisper('"+ data.from +"')\">&lt;"+ data.from +"&gt;</a> Whispers: ";
+		}
+		msg += data.message;
+		self.mes(msg,true);
+		// Play audio
+		if (data.to !== self.user.name) {
+			self.playAudio();
+		}
+		// Scroll down the chat and set focus to input.
+		self.setFocus();
+	});
+
+	// message / chat
+	self.socket.on('message', function(data) {
+		chat.debug('Chat:');
+		chat.debug(data);
+		var d = new Date(data.date),
+			hours = d.getHours(),
+			minutes = d.getMinutes(),
+			seconds = d.getSeconds();
+
+		var msg = "["+ self.twoDigits(hours) + ":" + self.twoDigits(minutes) + ":" + self.twoDigits(seconds) +"] ";
+			msg += "<a href=\"javascript:void(0)\" class=\"nickname\" onclick=\"chat.setWhisper('"+ data.name +"')\">&lt;"+ data.name +"&gt;</a> ";
+			msg += data.message;
+
+		self.mes(msg);
+		// Play audio
+		if (data.name !== self.user.name) {
+			self.playAudio();
+		}
+	});
+
+	// Get all messages from channel
+	self.socket.on('get-messages', function (data) {
+		chat.debug('Get-messages:');
+		chat.debug(data);
+		self.mes('Start of messages.');
+
+		$.each(data.messages, function() {
+			var d = new Date( this.date ),
+				hours = d.getHours(),
+				minutes = d.getMinutes(),
+				seconds = d.getSeconds();
+
+			var msg = "["+ self.twoDigits(hours) + ":" + self.twoDigits(minutes) + ":" + self.twoDigits(seconds) +"] ";
+			msg += "<a href=\"javascript:void(0)\" class=\"nickname\" onclick=\"chat.setWhisper('"+ this.name +"')\">&lt;"+ this.name +"&gt;</a> ";
+			msg += this.message;
+
+			self.mes(msg);
+		});
+
+		self.mes('End of messages.');
+	});
+
+	return this;
+};
+
+/*
+* Socket submit data
+* @require: elementId
+*/
+chat.setSubmit = function(elementId) {
+	var self = this;
+	var messageStr = elementId.val().trim();
+
+	if (messageStr.length) {
+		if (messageStr.trim().length > 1000) {
+			$.fn.nNotice({
+				message: "Message is too long! 1000 letters max.",
+				enableBackground: true
+			});
+		}
+		else {
+			// Check user commands
+			var isCommand = false;
+
+			// Check commands 1
+			var regex = /^(\/.*)$/g;
+			var command = regex.exec(messageStr);
+			if (command) {
+				isCommand = true;
+				switch (command[1]) {
+					// Show help
+					case "/h":
+					case "/help":
+					case "/commands":
+						self.mes("Commands:\n /h, /help, /commands - Display help.\n /log, /messages - Display logged channel messages.\n /c, /channel &lt;name&gt; - Change channel.\n /n, /name, /nick &lt;name&gt; - Change name.");
+					break;
+
+					// Log/Get messages
+					case "/log":
+					case "/messages":
+						//var channel = command[2] === undefined ? null : command[2].trim();
+						self.socket.emit('get-messages', { message: true });
+					break;
+
+					// Command depth: 2
+					default:
+						var regex = /^(\/.*)\s(.*)$/g;
+						var command = regex.exec(messageStr);
+						if (command) {
+							switch (command[1]) {
+								// Channel change
+								case "/c":
+								case "/channel":
+									var channel = command[2] === undefined ? null : command[2].trim();
+									if (channel) {
+										self.setChannel( channel );
+									}
+								break;
+
+								// Name change
+								case "/n":
+								case "/name":
+								case "/nick":
+									var name = command[2] === undefined ? null : command[2].trim();
+									if (name) {
+										self.setName( name );
+									}
+								break;
+							}
+						} else {
+							self.mes('Unknown command or missing options.');
+						}
+					break;
+				}
+			}
+			// Message was not a command. Send normal data
+			if (!isCommand) {
+				self.socket.emit('message', { message: messageStr });
+			}
+		}
+		elementId.val(null);
+		/* else {
+			self.socket.emit('message', { message: messageStr });
+			elementId.val(null);
+		} */
+	}
+
+	self.setFocus();
+
+	return this;
+};
+
+/*
+* menuAddUser - build user menu
+* @require user: id, name, channel
+*/
+chat.menuAddUser = function(id, name, channel) {
+	var self = this, active = 0;
+
+	// New menu object
+	var $menu = $('<ul/>');
+
+	// New user object
+	var $user = $('<li/>', {
+		class: 'user',
+		id: id,
+		title: name
+	}).html('&rsaquo; '+ name);
+
+	$user.click(function() {
+		active++;
+		if (active === 1) {
+			if (self.user.name === name) { //localStorage.name === name
+				var $menuSub = $('<ul/>');
+
+				// Whisper
+				$('<li/>', {
+					class: 'menu'
+				}).click(function() {
+					chat.setWhisper();
+				}).html('&lsaquo; New whisper').appendTo($menuSub);
+
+				// Change name
+				$('<li/>', {
+					class: 'menu'
+				}).click(function() {
+					self.setName();
+				}).html('&lsaquo; Change name').appendTo($menuSub);
+
+				// Change channel
+				$('<li/>', {
+					class: 'menu'
+				}).click(function() {
+					self.setChannel();
+				}).html('&lsaquo; Change channel').appendTo($menuSub);
+
+				// Add to user element
+				$($menuSub).appendTo($user);
+			}
+			else {
+				var $menuSub = $('<ul/>');
+				// Whisper menu
+				$('<li/>', {
+					class: 'menu'
+				}).click(function() {
+					self.setWhisper(name);
+				}).html('&lsaquo; Whisper').appendTo($menuSub);
+
+				// Append to user
+				$($menuSub).appendTo($user);
+			}
+		}
+	}).mouseleave(function() {
+		$(this).find('ul').remove();
+		active = 0;
+	});
+
+	// Append user to menu
+	$user.appendTo($menu);
+
+	// Display new menu
+	$menu.appendTo("#users");
+
+	chat.debug('menuAddUser: '+ name);
+
+	// Append to parent div
+	return this;
+};
+
+/*
+* Remove user from menu
+* @require userId
+*/
+chat.menuRemoveUser = function(userId) {
+	//var self = this;
+
+	// User menu array
+	var $menuArray = $("#users").find("ul").find("#" + userId);
+
+	// If array has any value, remove it
+	if ($menuArray.length) {
+		// Found in array: Remove user element:
+		$menuArray.parent().remove();
+		chat.debug('User menu remove');
+	}
+
+	return this;
+};
+
+/*
+* Clear menu
+* Remove all users from menu
+*/
+chat.menuClear = function() {
+	$('#users').find("ul").remove();
+
+	return this;
+};
+
+/*
+* Update user list & menu
+* @require user: id, name, channel
+*/
+chat.menuUpdateUser = function(id, name, channel) {
+	chat.debug('Menu update user');
+	var self = this;
+
+	// Remove old
+	self.menuRemoveUser(id);
+
+	// Build new user menu
+	self.menuAddUser(id, name, channel);
+
+	return this;
+};
+
+/*
+* Update users list & menu
+* @require user object { id, name, channel }
+*/
+chat.updateUsers = function(data) {
+	var self = this;
+
+	// Start from fresh element. Remove old menu:
+	$("#users").find("ul").remove();
+
+	// Add all users in array
+	$.each(data.users, function() {
+		var id = this.id,
+			name = this.name,
+			channel = this.channel,
+			active = 0;
+
+		// Build new user menu
+		self.menuAddUser(id, name, channel);
+
+	});
+
+	return this;
+};
+
+/*
+* UI open server connection
+* @optional: callback
+*/
+chat.connect = function(callback) {
+	var self = this;
+
+	$.fn.nConfirm({
+		title: "Welcome to Chat",
+		message: "Do you wish to <b>connect</b> to the server?",
+		enableBackground: true,
+		onSubmit: function() {
+			// Initialize
+			chat.init();
+			// callback fucntion
+			if (callback && typeof(callback) === "function") {
+				callback();
+			}
+		}
+	});
+
+	return this;
+};
+
+/*
+* UI open set nickname window
+* @require: nameStr -optional
+*/
+chat.setName = function(nameStr) {
+	if (nameStr === undefined) {
+		$.fn.nPrompt({
+			title: "Set Name",
+			message: "Please enter your <b>name</b> at the field below.",
+			value: (localStorage.name ? localStorage.name : chat.user.name),
+			enableBackground: false,
+			onSubmit: function(str) {
+				if (str !== null) {
+					chat.user.name = str;
+					localStorage.name = str;
+					chat.socket.emit('setName', { name: str });
+					chat.setFocus();
+				}
+			}
+		});
+	}
+	else {
+		if (nameStr.length < 2) {
+			$.fn.nNotice({
+				message: "Name is too short! 2 min.",
+				enableBackground: true
+			});
+		} else if (nameStr.length > 25) {
+			$.fn.nNotice({
+				message: "Name is too long! 25 max.",
+				enableBackground: true
+			});
+		} else {
+			chat.user.name = nameStr;
+			localStorage.name = nameStr;
+			chat.socket.emit('setName', { name: nameStr });
+			chat.setFocus();
+		}
+	}
+
+	return this;
+};
+
+/*
+* UI open change channel window
+* @require: channelName -optional
+*/
+chat.setChannel = function(channelName) {
+	var self = this;
+
+	if (channelName === undefined) {
+		$.fn.nPrompt({
+			title: "Set Channel",
+			message: "Please enter <b>channel</b> name at the field below.",
+			value: "General",
+			enableBackground: false,
+			onSubmit: function(str) {
+				if (str !== null) {
+					self.socket.emit('setChannel', { channel: str.toLowerCase() });
+				}
+			}
+		});
+	}
+	else {
+		if (channelName.length < 2) {
+			$.fn.nNotice({
+				message: "Channel name is too short! 2 min.",
+				enableBackground: true
+			});
+		} else if (channelName.length > 50) {
+			$.fn.nNotice({
+				message: "Channel name is too long! 50 max.",
+				enableBackground: true
+			});
+		} else {
+			// Send.
+			self.socket.emit('setChannel', { channel: channelName.toLowerCase() });
+		}
+	}
+
+	return this;
+};
+
+/*
+* UI open whisper window
+* @require: nickname, messageStr
+*/
+chat.setWhisper = function(nickname, messageStr) {
+	var self = this;
+
+	if (nickname === undefined || nickname === chat.user.name) {
+		$.fn.nPrompt({
+			title: "Send whisper",
+			message: "Please enter a whisper <b>name</b>.",
+			value: chat.user.whisper ? chat.user.whisper: '',
+			onSubmit: function(str) {
+				// Don't send to your self
+				if (str !== chat.user.name) {
+					$.fn.nPrompt({
+						title: "Send whisper",
+						message: "Please enter your <b>message</b> at the field below.",
+						onSubmit: function(message) {
+							self.sendWhisper(str, message);
+						}
+					});
+				}
+			}
+		});
+	}
+	// Dont send to your self
+	else if (nickname.length && nickname !== chat.user.name) {
+		if (nickname.length && messageStr === undefined) {
+			$("#sendform").find("#whisper").val(nickname);
+			self.setFocus();
+		}
+		else if (messageStr === '') {
+			$.fn.nPrompt({
+				title: "Send whisper",
+				message: "Please enter your <b>message</b> at the field below.",
+				onSubmit: function(message) {
+					self.sendWhisper(nickname, message);
+				}
+			});
+		}
+		else if (nickname.length && messageStr.length) {
+			self.sendWhisper(nickname, messageStr);
+		}
+	}
+
+	return this;
+};
+
+/*
+* Submit whisper data
+* @require: nickname, messageStr
+*/
+chat.sendWhisper = function(nickname, messageStr) {
+	var self = this;
+
+	if (nickname.length && messageStr.length) {
+		if (messageStr.length > 1000) {
+			$.fn.nNotice({
+				message: "Message is too long! 1000 letters max.",
+				enableBackground: true
+			});
+		}
+		else {
+			self.socket.emit('whisper', {
+				to: nickname,
+				from: chat.user.name,
+				message: messageStr
+			});
+			$("#sendform").find("#whisper").val(nickname);
+			self.setFocus();
+		}
+	}
+
+	return this;
+};
+
+/*
+* Check if post is command
+* @https://regex101.com/
+*/
+chat.isCommand = function(str, reg) {
+	// starts: /any-letter any-letter any-letter
+	//var regex = /^(\/.*)\s(.*)\s(.*)$/g;
+	var regex = reg || /^(\/.*)$/;
+
+	return regex.exec(str);
+};
+
+/*
+* Append message to the chat window
+* @require: message, whisper(true/false)
+*/
+chat.mes = function(message, whisper) {
+	var self = this;
+
+	if(whisper !== undefined || whisper === true) {
+		var msg = "<li><span class=\"whisper\">" + message + "</span></li>";
+	} else {
+		var msg = "<li><span class=\"message\">" + message + "</span></li>";
+	}
+	$("#messages").append(msg).linkify();
+	self.scrollDown();
+
+	return this;
+};
+
+/*
+* Focus on chat input
+* @require: elementId
+*/
+chat.setFocus = function(elementId) {
+	if (elementId === undefined) {
+		$("#message").focus();
+	} else {
+		$(elementId).focus();
+	}
+
+	return this;
+};
+
+/*
+* Scroll down the chat window
+* @require: elementId, animationSpeed
+*/
+chat.scrollDown = function(elementId, animationSpeed) {
+	if (elementId === undefined) {
+		var element = $("#content");
+	} else {
+		var element = $(elementId);
+	}
+	//var element = document.getElementById(elementId);
+	//element.scrollTop = element.scrollHeight;
+	var scrollHeight = element[0].scrollHeight;
+	if (animationSpeed === undefined) {
+		element.scrollTop( scrollHeight );
+	} else {
+		element.animate({
+			scrollTop: scrollHeight
+		}, animationSpeed);
+	}
+
+	return this;
+};
+
+/*
+* Play sound
+* @require: none
+*/
+chat.soundOn = false;
+chat.playAudio = function() {
+	var self = this;
+
+	var audio = document.getElementsByTagName("audio")[0];
+	if (audio !== undefined && self.soundOn) {
+		audio.play();
+	}
+
+	return this;
+};
+
+/*
+* Sound settings
+* @require: none
+*/
+chat.playSettings = function(active) {
+	var self = this;
+
+	if (active === undefined) {
+		self.soundOn = self.soundOn ? false : true;
+	} else {
+		self.soundOn = active;
+	}
+	self.mes("Sound is set: " + (self.soundOn ? 'on' : 'off'));
+	self.playAudio();
+
+	return this;
+};
+
+/*
+* Sound volume
+* @require: level
+*/
+chat.playVolume = function(level) {
+	$("audio").volume = level;
+
+	return this;
+};
+
+/*
+* Helper function: return in two digits string
+* @require: string
+*/
+chat.twoDigits = function(string) {
+	return ("0" + string).slice(-2);
+};
+
+/*
+* Quit/Close socket
+* @require: none
+*/
+chat.quit = function() {
+	var self = this;
+
+	if (socket != null) {
+		chat.debug("Socket close");
+		self.socket.close();
+		socket=null;
+	}
+
+	return this;
+}
+
+/*
+* Reconnect socket
+* @require: none
+*/
+chat.reconnect = function() {
+	var self = this;
+
+	self.quit();
+	setTimeout(function(){
+		chat.debug("Socket init");
+		self.init();
+	},2000);
+
+	return this;
+}
+
+/*
+* Make main menu
+* @require: string, object
+*/
+chat.mainMenu = function(event, object) {
+	// Create main menu
+	var menuHolder = $('<div/>', {
+		id: 'myMenu'
+	}).css({
+		position: 'fixed',
+		top: '0.5%',
+		left: '0.5%',
+		width: '35px',
+		height: '35px',
+		background: '#ccc',
+		opacity: '0.50',
+		zIndex: '+1'
+	});
+
+	// Create canvas
+	var mainMenu = $('<canvas/>', {
+		width: '35px',
+		height: '35px'
+	}).appendTo(menuHolder);
+	
+		// Draw canvas
+	mainMenu.each(function() {
+		var self = this,
+			x = self.width,
+			y = self.height,
+			ctx = self.getContext("2d");
+
+		//Lets go...
+		ctx.fillStyle = "#FF0000";
+		ctx.lineCap = "round";
+		//Background
+		var my_gradient = ctx.createLinearGradient(0, 0, 0, y);
+		my_gradient.addColorStop(0, "gray");
+		my_gradient.addColorStop(1, "white");
+		ctx.fillStyle = my_gradient;
+		ctx.fillRect(0, 0, x, y);
+		//Border
+		ctx.lineWidth = 4 * y / 100;
+		ctx.strokeRect(2 * y / 100, 2 * y / 100, x - 4 * y / 100, y - 4 * y / 100);
+		//Lines
+		ctx.lineWidth = 8 * y / 100;
+		ctx.moveTo(10 * x / 100, 25 * y / 100);
+		ctx.lineTo(90 * x / 100, 25 * y / 100);
+		ctx.moveTo(10 * x / 100, 50 * y / 100);
+		ctx.lineTo(90 * x / 100, 50 * y / 100);
+		ctx.moveTo(10 * x / 100, 75 * y / 100);
+		ctx.lineTo(90 * x / 100, 75 * y / 100);
+		ctx.scale(0.8, 1.0);
+		ctx.stroke();
+	});
+
+	menuHolder.mouseenter(function() {
+		$(this).css({
+			opacity: '1.0'
+		});
+	}).mouseleave(function() {
+		$(this).css({
+			opacity: '0.50'
+		});
+	});
+
+	menuHolder.dropdown(event, object);
+
+	// Display
+	menuHolder.appendTo('body');
+
+	return this;
+};
---
+++ nw-app.js
@@ -0,0 +1,124 @@
+/*
+* Script.js - node app
+*
+* Export functions you can use on index.html
+* @global - all stuff
+* @process.mainModule.exports.myFunctionName()
+*/
+(function(){
+
+	// Count page loadtimes
+	exports.pageloads = 0;
+	exports.pageload = function () {
+		exports.pageloads = exports.pageloads + 1;
+		console.log(exports.pageloads + ": pageloads.");
+		//window.location - this would return current filename and line
+	};
+
+    // Start your own chat server @ localhost:9000
+    exports.server = {};
+    exports.server.online = false;
+    exports.server.start = function(callback) {
+        //var gui = require('nw.gui') || global.window.nwDispatcher.requireNwGui();
+        require('../server.js');
+
+        console.log('Server started.');
+		exports.server.online = true;
+
+        // callback function
+        if (callback && typeof(callback) === "function") {
+            callback();
+        }
+
+        return this;
+    };
+
+    // File system: read
+    // Example: exports.file.read('package.json',function(e){console.log(e);});
+    exports.readFile = function(fileName, callback) {
+        fs = require('fs');
+        fileText = fs.readFileSync(fileName).toString(); // 'foo.txt'
+
+        // callback function
+        if (callback && typeof(callback) === "function") {
+            callback(fileText);
+        }
+
+        return fileText;
+    };
+
+    // read dir: fs.readdirSync('.')
+
+    // File system: write
+    exports.writeFile = function(fileName, fileText, callback) {
+        fs = require('fs');
+        fs.writeFileSync(fileName, fileText); // 'bar.txt'
+
+        // callback function
+        if (callback && typeof(callback) === "function") {
+            callback();
+        }
+
+        return this;
+    };
+
+})();
+
+// Self-called functions: Will be executed on start
+/*
+(function(){
+    // Menu
+    var gui = require('nw.gui');
+    var win = gui.Window.get();
+
+    var rootMenu = new gui.Menu({
+        type: 'menubar'});
+    var myMenu = new gui.Menu();
+
+    myMenu.append(new gui.MenuItem({
+        type: 'normal',
+        label: 'Debug',
+        click: function (){
+            win.showDevTools();
+        } }));
+
+    rootMenu.append(new gui.MenuItem({
+        label: 'NW-Reveal',
+        submenu: myMenu
+    }));
+
+    rootMenu.append(new gui.MenuItem({
+        label: 'NW-Reveal',
+        submenu: myMenu
+    }));
+
+    // Tray icon
+    var tray = new gui.Tray({
+        icon: 'icon.png'
+      });
+    var menu = new gui.Menu();
+    menu.append(new gui.MenuItem({
+        type: 'checkbox',
+        label: 'Always-on-top',
+        click: function () {...}
+      }));
+    tray.menu = menu;
+
+    // Page load times
+    exports.pageload();
+
+    // Other options
+    var gui = require('nw.gui');
+    var win = gui.Window.get();
+
+    win.hide();
+    win.show();
+    win.maximize();
+    win.minimize();
+
+    window.open();
+    window.moveBy(10,30);
+    window.resizeTo(800,600);
+
+})(0);
+*/
