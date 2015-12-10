a0c93cf6c7089711325e93f69132ec9581a83fb3
Sehrentos <sehrentos@gmail.com>
2015-12-10 11:14:35 +0200

    Added window.frame check
    Added if check for package.json window.frame.
    If frame is not in use we will enable custom windowOption buttons.
    Converted some spaces into TABs.

--- data/nw-app.js
+++ data/nw-app.js
@@ -13,110 +13,110 @@
 		exports.pageloads = exports.pageloads + 1;
 	};
 
-    // Start your own chat server @ localhost:9000
-    exports.server = {};
-    exports.server.online = false;
-    exports.server.start = function(callback) {
-        //var gui = require('nw.gui') || global.window.nwDispatcher.requireNwGui();
-        require('./server.js');
+	// Start your own chat server @ localhost:9000
+	exports.server = {};
+	exports.server.online = false;
+	exports.server.start = function(callback) {
+		//var gui = require('nw.gui') || global.window.nwDispatcher.requireNwGui();
+		require('./server.js');
 
-        console.log('Server started.');
+		console.log('Server started.');
 		exports.server.online = true;
 
-        // callback function
-        if (callback && typeof(callback) === "function") {
-            callback();
-        }
+		// callback function
+		if (callback && typeof(callback) === "function") {
+			callback();
+		}
 
-        return this;
-    };
+		return this;
+	};
 
-    // File system: read
-    // Example: exports.file.read('package.json',function(e){console.log(e);});
-    exports.readFile = function(fileName, callback) {
-        fs = require('fs');
-        fileText = fs.readFileSync(fileName).toString(); // 'foo.txt'
+	// File system: read
+	// Example: exports.file.read('package.json',function(e){console.log(e);});
+	exports.readFile = function(fileName, callback) {
+		fs = require('fs');
+		fileText = fs.readFileSync(fileName).toString(); // 'foo.txt'
 
-        // callback function
-        if (callback && typeof(callback) === "function") {
-            callback(fileText);
-        }
+		// callback function
+		if (callback && typeof(callback) === "function") {
+			callback(fileText);
+		}
 
-        return fileText;
-    };
+		return fileText;
+	};
 
-    // read dir: fs.readdirSync('.')
+	// read dir: fs.readdirSync('.')
 
-    // File system: write
-    exports.writeFile = function(fileName, fileText, callback) {
-        fs = require('fs');
-        fs.writeFileSync(fileName, fileText); // 'bar.txt'
+	// File system: write
+	exports.writeFile = function(fileName, fileText, callback) {
+		fs = require('fs');
+		fs.writeFileSync(fileName, fileText); // 'bar.txt'
 
-        // callback function
-        if (callback && typeof(callback) === "function") {
-            callback();
-        }
+		// callback function
+		if (callback && typeof(callback) === "function") {
+			callback();
+		}
 
-        return this;
-    };
+		return this;
+	};
 
 })();
 
 // Self-called functions: Will be executed on start
 /*
 (function(){
-    // Menu
-    var gui = require('nw.gui');
-    var win = gui.Window.get();
+	// Menu
+	var gui = require('nw.gui');
+	var win = gui.Window.get();
 
-    var rootMenu = new gui.Menu({
-        type: 'menubar'});
-    var myMenu = new gui.Menu();
+	var rootMenu = new gui.Menu({
+		type: 'menubar'});
+	var myMenu = new gui.Menu();
 
-    myMenu.append(new gui.MenuItem({
-        type: 'normal',
-        label: 'Debug',
-        click: function (){
-            win.showDevTools();
-        } }));
+	myMenu.append(new gui.MenuItem({
+		type: 'normal',
+		label: 'Debug',
+		click: function (){
+			win.showDevTools();
+		} }));
 
-    rootMenu.append(new gui.MenuItem({
-        label: 'NW-Reveal',
-        submenu: myMenu
-    }));
+	rootMenu.append(new gui.MenuItem({
+		label: 'NW-Reveal',
+		submenu: myMenu
+	}));
 
-    rootMenu.append(new gui.MenuItem({
-        label: 'NW-Reveal',
-        submenu: myMenu
-    }));
+	rootMenu.append(new gui.MenuItem({
+		label: 'NW-Reveal',
+		submenu: myMenu
+	}));
 
-    // Tray icon
-    var tray = new gui.Tray({
-        icon: 'icon.png'
-      });
-    var menu = new gui.Menu();
-    menu.append(new gui.MenuItem({
-        type: 'checkbox',
-        label: 'Always-on-top',
-        click: function () {...}
-      }));
-    tray.menu = menu;
+	// Tray icon
+	var tray = new gui.Tray({
+		icon: 'icon.png'
+	  });
+	var menu = new gui.Menu();
+	menu.append(new gui.MenuItem({
+		type: 'checkbox',
+		label: 'Always-on-top',
+		click: function () {...}
+	  }));
+	tray.menu = menu;
 
-    // Page load times
-    exports.pageload();
+	// Page load times
+	exports.pageload();
 
-    // Other options
-    var gui = require('nw.gui');
-    var win = gui.Window.get();
+	// Other options
+	var gui = require('nw.gui');
+	var win = gui.Window.get();
 
-    win.hide();
-    win.show();
-    win.maximize();
-    win.minimize();
+	win.hide();
+	win.show();
+	win.maximize();
+	win.minimize();
 
-    window.open();
-    window.moveBy(10,30);
-    window.resizeTo(800,600);
+	window.open();
+	window.moveBy(10,30);
+	window.resizeTo(800,600);
 
 })(0);
 */
